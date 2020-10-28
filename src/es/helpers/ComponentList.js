f9d9bacaf6a2d309c7f2afc3bb69bd8b95c93cb1
TailorMadeCode <69360983+tailormadecode@users.noreply.github.com>
2020-10-28 17:50:10 +0100

    feat: add helper function

--- src/es/helpers/ComponentList.js
+++ src/es/helpers/ComponentList.js
@@ -1,50 +1,26 @@
 // @ts-check
 
-/* global customElements */
-
 /**
- * This global class creates the WebComponent and the List View in the Browser
- *
- * @class ComponentList
+ * This global helper function creates the List View in the Browser
  */
 
-export default class ComponentList {
-  constructor ({ name, filePath }) {
-    this.webComponent = name
-    this.filePath = filePath
+export default function (name) {
+  if (!name) return
 
-    if (!this.webComponent || !this.filePath) throw new Error('TypeError')
-
-    this.createComponent()
-  }
-
-  /**
-   * First level controllers and organisms are loaded and defined here (loadChildComponents)
-   */
-  createComponent () {
-    import(this.filePath).then(module => [this.webComponent, module.default]).then(element => {
-      // don't define already existing customElements
-      if (element && !customElements.get(element[0])) customElements.define(...element)
-      this.createList()
-    })
-  }
-
-  createList () {
-    Array.from(document.querySelectorAll('dt')).forEach(dt => {
-      const dd = document.createElement('dd')
-      if (dt.querySelector('style')) {
-        const codeCSS = document.createElement('code')
-        codeCSS.classList.add('code-css')
-        codeCSS.textContent = dt.querySelector('style').textContent
-        dd.appendChild(codeCSS)
-      }
-      if (dt.querySelector(this.webComponent)) {
-        const codeHTML = document.createElement('code')
-        codeHTML.classList.add('code-html')
-        codeHTML.textContent = dt.querySelector(this.webComponent).outerHTML.replace(/=""/g, '').replace(/\n[\s]{8}/, '\n  ').replace(/\n[\s]*?(<\/)/, '\n$1')
-        dd.appendChild(codeHTML)
-      }
-      dt.after(dd)
-    })
-  }
+  Array.from(document.querySelectorAll('dt')).forEach(dt => {
+    const dd = document.createElement('dd')
+    if (dt.querySelector('style')) {
+      const codeCSS = document.createElement('code')
+      codeCSS.classList.add('code-css')
+      codeCSS.textContent = dt.querySelector('style').textContent
+      dd.appendChild(codeCSS)
+    }
+    if (dt.querySelector(name)) {
+      const codeHTML = document.createElement('code')
+      codeHTML.classList.add('code-html')
+      codeHTML.textContent = dt.querySelector(name).outerHTML.replace(/=""/g, '').replace(/\n[\s]{8}/, '\n  ').replace(/\n[\s]*?(<\/)/, '\n$1')
+      dd.appendChild(codeHTML)
+    }
+    dt.after(dd)
+  })
 }
