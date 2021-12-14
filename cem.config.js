c7a9c04825c4f50f72e7942b421a66225b530df9
edmgb <evangelos.dinopoulos@mgb.ch>
2021-12-14 09:47:05 +0100

    chore: beautify config file

--- cem.config.js
+++ cem.config.js
@@ -1,7 +1,5 @@
 export default {
   globs: ['src/es/components/**/*.js'],
-  exclude: [
-    'src/es/components/prototypes/*.js'
-  ],
+  exclude: ['src/es/components/prototypes/*.js'],
   outdir: 'doc'
 }
